bfadc7fc27e36d9a9da38f6c7df18f1444d32fbe
Lawrence <lawrencehome21@gmail.com>
2023-09-09 20:27:03 -0700

    set some database attribute

--- config.php
+++ config.php
@@ -5,4 +5,8 @@ $databaseName = "API";
 
 $connect = new PDO('mysql:host=127.0.0.1;dbname='.$databaseName.';charset=utf8',$userName,$password);
 
+// set some db attribute
+$connect->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$connect->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
+$connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
