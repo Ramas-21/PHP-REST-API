56d190bcfa29e9f9aee8e98f947a83d0ce02cd54
Lawrence <lawrencehome21@gmail.com>
2023-09-10 07:22:03 -0700

    counted the number of rows

--- api/read.php
+++ api/read.php
@@ -6,4 +6,29 @@ header('Content-type: application/json');
 include_once('../core/initialize.php');
 
 $post = new Post($connect);
+
+// blog post query
+$result = $post->read();
+// getting the row count
+$num = $result->rowCount();
+if($num > 0){
+    $post_arr = array();
+    $post_arr['data'] = array();
+
+    while($row = $result->fetch(PDO::FETCH_ASSOC)){
+        extract($row);
+        $post_item = array(
+            `id` => $id,
+            `title` => $title,
+            `body` => html_entity_decode($body),
+            `author` => $author,
+            `category_id` => $category_id,
+            `category_name` => $category_name
+        );
+        array_push($post_arr['data'], $post_item);
+    }
+}
+else{
+
+}
 ?>
