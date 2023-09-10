4f071447fbcd5a9fb5ddb99c8682eedb92466891
Lawrence <lawrencehome21@gmail.com>
2023-09-10 06:41:51 -0700

    created the post properties

--- core/post.php
+++ core/post.php
@@ -3,6 +3,15 @@
 class Post {
     // Database stuff
     private $connect;
-    
+    private $table = 'posts';
+
+    //post properties
+    public $id;
+    public $category_id;
+    public $category_name;
+    public $title;
+    public $body;
+    public $author;
+    public $create_date;
 }
 ?>
