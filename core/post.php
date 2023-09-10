1cdee7a2d44572215a0c7ccb502c532a5c952011
Lawrence <lawrencehome21@gmail.com>
2023-09-10 06:56:38 -0700

    creted a read function

--- core/post.php
+++ core/post.php
@@ -12,13 +12,27 @@ class Post {
     public $title;
     public $body;
     public $author;
-    public $create_date;
+    public $created_at;
 
     // constructor with DB connection
     public function __construct($connect){
         $this->conn = $connect;
     }
-
-    public function read()
+    // getting post from the database
+    public function read(){
+        $query = 'SELECT
+        c.name as category_name,
+        p.id,
+        p.category_id,
+        p.title,
+        p.body,
+        p.author,
+        p.created_at
+        FROM
+        ` .$this->table . ` p
+        LEFT JOIN
+        categories c ON p.category_id = c.id
+        ORDER BY p.created_at';
+    }
 }
 ?>
