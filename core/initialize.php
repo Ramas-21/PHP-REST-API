c3976a16dc4eed8a50b509b725db7634c46eb776
Lawrence <lawrencehome21@gmail.com>
2023-09-10 06:33:45 -0700

    loaded the config file first

--- core/initialize.php
+++ core/initialize.php
@@ -4,4 +4,7 @@ defined('SITE_ROOT') ? null : define('SITE_ROOT', DS . 'wamp64'.DS.'www'.DS.'php
 
 defined('INC_PATH') ? null : define('INC_PATH', SITE_ROOT.DS.'includes');
 defined('CORE_PATH') ? null : define('CORE_PATH', SITE_ROOT.DS.'core');
+
+// load the config file first
+require_once(INC_PATH.DS."config.php");
 ?>
