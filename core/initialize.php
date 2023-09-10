5d227a0023ca8d2dfc122cadbfb2217ecd337dd2
Lawrence <lawrencehome21@gmail.com>
2023-09-10 06:31:45 -0700

    defined the core directory as a SITE_ROOT

--- core/initialize.php
+++ core/initialize.php
@@ -3,4 +3,5 @@ defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 defined('SITE_ROOT') ? null : define('SITE_ROOT', DS . 'wamp64'.DS.'www'.DS.'php_REST_API');
 
 defined('INC_PATH') ? null : define('INC_PATH', SITE_ROOT.DS.'includes');
+defined('CORE_PATH') ? null : define('CORE_PATH', SITE_ROOT.DS.'core');
 ?>
